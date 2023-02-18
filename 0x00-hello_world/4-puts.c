41bafade6841cff3d83ac40cb7b9c3176afb2851
zaki-prod <zakiprod04@gmail.com>
2023-02-18 04:06:28 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- * main - main block 
+ * main - Entry point  
  *
- * return: 0
- * */
- int main(void)
+ * Return: Always 0 (success)
+ */
+int main(void)
 {
-     puts("\"Programming is like building a multilingual puzzle");
-     return (0);
-}     
+
+        puts("\""Programming is like building a multilingual puzzle");
+	
+	return (0);
+}	
