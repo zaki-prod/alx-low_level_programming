67e50f090eae23724b154d3944a140e58deffdf6
zaki-prod <zakiprod04@gmail.com>
2023-02-19 01:36:38 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
+
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
+ * * Return: Always 0 
  */
+
+
 int main(void)
 {
     printf("with proper grammar, but the outcome is a piece of art,\n")	
