53e567f8bb064590590386a400b5422b5ba9a939
zaki-prod <zakiprod04@gmail.com>
2023-02-20 13:27:06 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,8 @@
  * , followed by a new line
  * Return: Always 0 
  */
-
-
 int main(void)
-
-
 {
-    puts("\"Programming is like building a multilingual puzzle");
-
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
