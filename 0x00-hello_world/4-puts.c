ca939f278d33832a20f52e65c1d913e666e13d7b
zaki-prod <zakiprod04@gmail.com>
2023-02-20 13:33:42 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - prints "Programming is like building a multilingual puzzle
- * , followed by a new line
- * Return: Always 0 
+ * main - prints "Programming is like building a multilingual puzzl
+ *
+ * Return: Always
  */
 int main(void)
 {
