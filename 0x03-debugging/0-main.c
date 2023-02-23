6380a293f72fcf0f4a4663119df5baf7ca8b3425
zaki-prod <zakiprod04@gmail.com>
2023-02-23 12:28:12 -0800

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - Test function for positive or negative
  * Return: 0
@@ -6,9 +7,10 @@
 
 int main(void)
 {
-	int i
-		i = 0;
+	int i;
+i = 0;
 	positive_or_negative(i);
 
 return (0);
 }
+
