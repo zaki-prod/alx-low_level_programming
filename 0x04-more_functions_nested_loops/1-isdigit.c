c6a5d4edfd1638229d7e83c2ddcc95f209d3a77a
zaki-prod <zakiprod04@gmail.com>
2023-02-24 13:06:13 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -14,25 +14,3 @@ int _isdigit(int c)
 	else
 		return (0);
 }
-
-
-1-main.c
-#include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char c;
-
-    c = '0';
-    printf("%c: %d\n", c, _isdigit(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
-    return (0);
-}
-
