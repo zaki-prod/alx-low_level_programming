c35f7f7d80f663d1525f8a516c43191acb83f687
zaki-prod <zakiprod04@gmail.com>
2023-04-06 14:00:02 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,8 +6,23 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-	   print_binary(n >> 1);
+	int i;
+	unsigned long int current;
+	int count = 0;
 
-	_putchar((n & 1) + '0');
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
 }
+
