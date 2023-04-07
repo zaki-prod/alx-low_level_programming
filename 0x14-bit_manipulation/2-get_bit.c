03c3bdeb916085337177395312342511ebd0c633
zaki-prod <zakiprod04@gmail.com>
2023-04-07 02:35:50 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,16 +8,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit_val;
-
 	if (index > 63)
 		return (-1);
 
-	bit_val = (n >> index) & 1;
-
-	printf("The value of bit %d is %d\n");
-
-	return (bit_val);
+	return ((n >> index) & 1);
 }
-
-
