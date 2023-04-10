361f9c0bd094dd3434776f054442a06b687b64fb
zaki-prod <zakiprod04@gmail.com>
2023-04-10 12:07:30 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,1 +1,32 @@
+/**
+ * create_file - Make a new file and write some text to it.
+ * @filename: The name of the file to create.
+ * @text_content:  The string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ *         Otherwise - 1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
 
