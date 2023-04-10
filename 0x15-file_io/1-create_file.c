383375e26e345f6bcaea1ff428e20cfd672f85ad
zaki-prod <zakiprod04@gmail.com>
2023-04-10 12:14:02 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,25 +8,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
-
 	if (filename == NULL)
-		return (-1);
+	return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+if (fd == -1)
+	return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+if (text_content != NULL)
+{
+	while (text_content[len])
+		len++;
 	w = write(fd, text_content, len);
-
-	if (fd == -1 || w == -1)
+	if (w == -1)
 		return (-1);
+}
 
-	close(fd);
+close(fd);
 
-	return (1);
+return (1);
 }
 
