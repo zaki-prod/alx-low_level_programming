0813171fa32b1e5b0471befe515fe76316904f3c
zaki-prod <zakiprod04@gmail.com>
2023-04-10 16:39:53 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * file_create - Creates a file.
+ * filename: A pointer to the name of the file to create.
+ * @text_contents: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
  *         Otherwise - 1.
