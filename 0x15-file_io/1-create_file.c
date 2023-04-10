4effae6a89d559dab52fbd75ce3b63e05feed783
zaki-prod <zakiprod04@gmail.com>
2023-04-10 16:30:25 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,9 +3,10 @@
  * @filename: The name of the file to create.
  * @text_content:  The string to write to the file.
  *
- * Return: 1 on success, -1 on failure.
+ * Return: 1 on success, -1 on fai
  */
 int create_file(const char *filename, char *text_content)
+
 {
 	int fd, w, len = 0;
 
